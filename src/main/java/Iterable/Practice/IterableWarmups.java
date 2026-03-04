2f333f8dfaf78d5c863909b7b8b8b47b1ed2f0c5
Derwin Bell <djbell261@gmail.com>
2026-03-04 18:08:28 -0500

    IteratorWarmups done

--- src/main/java/Iterable/Practice/IterableWarmups.java
+++ src/main/java/Iterable/Practice/IterableWarmups.java
@@ -15,8 +15,8 @@ public class IterableWarmups {
         numbers.add(8);
 
         System.out.println("Sum: " + sum(numbers));
-      //  System.out.println("Even count: " + countEven(numbers));
-      //  System.out.println("Max value: " + findMax(numbers));
+       System.out.println("Even count: " + countEven(numbers));
+       System.out.println("Max value: " + findMax(numbers));
     }
 
 
@@ -51,6 +51,12 @@ public class IterableWarmups {
         // TODO:
         // Loop through numbers
         // Increment count if number is even
+        for(Integer num : numbers){
+            if(num % 2 ==0){
+                count++;
+            }
+        }
+
 
         return count;
     }
@@ -67,6 +73,11 @@ public class IterableWarmups {
         // TODO:
         // Loop through numbers
         // Update max if current number is larger
+
+        for(Integer num : numbers){
+            max = Math.max(max, num);
+        }
+
 
         return max;
     }
