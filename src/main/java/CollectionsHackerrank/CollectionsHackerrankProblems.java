46e02ff49d919499936ec275ecad9a68ddbaa4ed
BennettDeveloper <bennettdeveloper@gmail.com>
2026-03-06 16:34:20 -0500

    Implemented Hackerrank Problems

--- src/main/java/CollectionsHackerrank/CollectionsHackerrankProblems.java
+++ src/main/java/CollectionsHackerrank/CollectionsHackerrankProblems.java
@@ -47,9 +47,19 @@ public class CollectionsHackerrankProblems {
         public static Map<Integer, Integer> countFrequency(List<Integer> numbers) {
 
             // TODO: Implement this method
+            HashMap<Integer, Integer> map = new HashMap<>();
 
+            for (int i = 0; i < numbers.size(); i++) {
+                int num = numbers.get(i);
 
-            return null;
+                if(map.containsKey(num)) {
+                    map.put(num, map.get(num) + 1);
+                }
+                else {
+                    map.put(num, 1);
+                }
+            }
+            return map;
         }
 
         /*
@@ -63,19 +73,26 @@ public class CollectionsHackerrankProblems {
         public static Integer firstUnique(List<Integer> numbers) {
 
             // TODO: Implement this method
+            LinkedHashMap<Integer, Integer> numberMap = new LinkedHashMap<>();
 
-            int onlyOnceNum = numbers.get(0);
-            boolean firstTime = false;
-
-            HashSet<Integer> numSet = new HashSet<Integer>();
-            List<Integer> resultList = new ArrayList<>();
-
-            for(int i = 0; i < numbers.size(); i++) {
-                numSet.add(numbers.get(i));
+            //populate hashmap
+            for(int num : numbers) {
+                if(numberMap.containsKey(num)) {
+                    numberMap.put(num, numberMap.get(num) + 1);
+                }
+                else {
+                    numberMap.put(num, 1);
+                }
             }
 
 
-
+            //find first unique num in LinkedHashMap (ordered by insertion)
+            for(int num : numberMap.keySet()) {
+                //1 means only appeared once, value is set to the # frequency in my hashMap
+                if(numberMap.get(num) <= 1) {
+                    return num;
+                }
+            }
             return null;
         }
 
@@ -92,8 +109,43 @@ public class CollectionsHackerrankProblems {
         public static boolean twoSum(List<Integer> numbers, int target) {
 
             // TODO: Implement this method
+            //Bad way, check list of numbers for every number we iterate through O(n^2)
+            /*
+            for(int i = 0; i < numbers.size(); i++) {
+                for(int j = 0; j < numbers.size(); j++) {
+                    if(numbers.get(i) + numbers.get(j) == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+            */
 
 
+            //Good (decent) way,
+            //build dict of known values, dont need to worry about value,
+            //want O(1) access of key!
+
+            HashMap<Integer,Integer> numMap = new HashMap<>();
+
+            for(int num : numbers) {
+                numMap.put(num, 1);
+            }
+
+            //iterate through array,
+            // originally we did arr[i] + arr[j] = target
+            //but I want to solve for arr[j] now.
+
+            //so its now gonna be abs(arr[i] - target) = arr[j]
+            for (int i = 0; i < numbers.size(); i++) {
+
+                int sumNeededValue = Math.abs(numbers.get(i) - target);
+                //if this value that we need for the target is
+                // somewhere in our list, return true.
+                if(numMap.containsKey(sumNeededValue)) {
+                    return true;
+                }
+            }
 
             return false;
         }
@@ -110,7 +162,33 @@ public class CollectionsHackerrankProblems {
 
             // TODO: Implement this method
 
-            return 0;
+            int count = 0;
+            //make hashMap
+            //iterate through list
+            //if not in hashmap, add to counter
+
+            HashMap<String, Integer> wordMap = new HashMap<>();
+
+            //Iterate through words, add to Map
+            for(String word : words) {
+                if (wordMap.containsKey(word)) {
+                    wordMap.put(word, wordMap.get(word) + 1);
+                }
+                else {
+                    wordMap.put(word, 1);
+                }
+            }
+
+            //then if its 1, increment counter;
+            for(int i = 0; i < words.size(); i++) {
+                String word = words.get(i);
+
+                if(wordMap.get(word) <= 1 ) {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         /*
@@ -125,7 +203,21 @@ public class CollectionsHackerrankProblems {
 
             // TODO: Implement this method
 
-            return null;
+            //convert queue to array
+            ArrayList<Integer> newList = new ArrayList<>();
+
+            //populate array with queue values
+            for(int i = 0; i < queue.size(); i++) {
+                newList.add(queue.poll());
+            }
+
+            //reverse list
+            newList.reversed();
+
+            //then back to Queue (LinkedList, cause Queue is an interface)
+            Queue<Integer> resultQueue = new LinkedList<>(newList);
+
+            return resultQueue;
         }
 
         /*
@@ -143,7 +235,20 @@ public class CollectionsHackerrankProblems {
 
             // TODO: Implement this method
 
-            return false;
+            int lCount = 0;
+            int rCount = 0;
+
+            //Count left parenthesis and right parenthesis, if they have equal count
+            //then they are balanced.
+            for(int i = 0; i < expression.length(); i++) {
+                if(expression.charAt(i) == '(') {
+                    lCount++;
+                }
+                else if (expression.charAt(i) == ')') {
+                    rCount++;
+                }
+            }
+            return lCount == rCount;
         }
 
         /*
@@ -159,7 +264,33 @@ public class CollectionsHackerrankProblems {
             // TODO: Implement this method
 
 
-            return null;
+            HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+            //Build HashMap, based on number frequency
+            for(int num : numbers) {
+                if(frequencyMap.containsKey(num)) {
+                    frequencyMap.put(num, frequencyMap.get(num) + 1);
+                }
+                else {
+                    frequencyMap.put(num, 1);
+                }
+            }
+
+            int maxValue = 0;
+            int mostFrequentNum = 0;
+
+            //Iterate through HashMap keys, find which one has the largest value
+            //return that value.
+
+            for(int key : frequencyMap.keySet()) {
+                //Max pattern
+                if(frequencyMap.get(key) > maxValue) {
+                    maxValue = frequencyMap.get(key);
+                    mostFrequentNum = key;
+                }
+            }
+
+            return mostFrequentNum;
         }
 
         /*
@@ -179,7 +310,25 @@ public class CollectionsHackerrankProblems {
 
             // TODO: Implement this method
 
-            return null;
+            HashMap<Integer, List<String>> lenGroup = new HashMap<>();
+
+            //build map.
+            for(String word: words) {
+
+                //Key is now the length
+                if(lenGroup.containsKey(word.length())) {
+                    List<String> getList = lenGroup.get(word.length());
+                    getList.add(word);
+                    lenGroup.put(word.length(), getList);
+                }
+                else {
+                    List<String> wordList = new ArrayList<>();
+                    wordList.add(word);
+                    lenGroup.put(word.length(),wordList);
+                }
+
+            }
+            return lenGroup;
         }
 
         /*
@@ -196,7 +345,55 @@ public class CollectionsHackerrankProblems {
 
             // TODO: Implement this method
 
-            return 0;
+            //initialize variables
+            int l = 0;
+            int r = 0;
+
+            int maxSum = 0;
+            int sum = 0;
+            boolean rWindowReached = false;
+
+            //minus the slided value, keep sum otherwise.
+            //like a train.
+
+            //while loop
+            while(r < numbers.size()) {
+
+                if(!rWindowReached) {
+                    //ad r value to sum, until r reaches k-1
+                    sum += r;
+                    r++;
+
+                    //check if r reaches k-1
+                    if(r >= k-1) {
+                        rWindowReached = true;
+
+                        //If new maxSum found, set it
+                        if(sum >= maxSum) {
+                            maxSum = sum;
+                        }
+                    }
+                }
+                else {
+
+                    //subtract left side of window
+                    sum -= numbers.get(l);
+                    l++;
+
+                    //add right side of window
+                    sum += numbers.get(r);
+                    r++;
+
+                    //If new maxSum found, set it
+                    if(sum >= maxSum) {
+                        maxSum = sum;
+                    }
+
+                }
+
+            }
+
+            return maxSum;
         }
     }
 }
