e1df3c93028ffc3039b6889a2f020ca1e6e1ede0
michaelmoss <mxmoss261@gmail.com>
2026-03-07 10:54:58 -0500

    java-collections

--- src/main/java/Queues/Deque/DequeProblems.java
+++ src/main/java/Queues/Deque/DequeProblems.java
@@ -1,13 +1,22 @@
 package Queues.Deque;
 
 import java.util.Deque;
+import java.util.ArrayDeque;
+
 
 public class DequeProblems {
     public static void main(String[] args) {
 
         // You can test your methods here
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        addFront(deque, 5);
+        addBack(deque, 10);
+
+        System.out.println(deque);
 
     }
+
 
     /*
     Problem 1
@@ -20,8 +29,10 @@ public class DequeProblems {
     public static void addFront(Deque<Integer> deque, int value) {
 
         // TODO: Implement this method
+        deque.addFirst(value);
 
     }
+
 
     /*
     Problem 2
@@ -34,8 +45,10 @@ public class DequeProblems {
     public static void addBack(Deque<Integer> deque, int value) {
 
         // TODO: Implement this method
+        deque.addLast(value);
 
     }
+
 
     /*
     Problem 3
@@ -96,5 +109,5 @@ public class DequeProblems {
 
         return null;
     }
-
 }
+
